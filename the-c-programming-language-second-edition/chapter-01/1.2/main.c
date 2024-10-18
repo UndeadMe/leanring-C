bc5209db307452030b1b0cbefbcd461b0e55f2e1
undeadme <arash.k021@yahoo.com>
2024-10-18 15:55:47 +0330

    feat: make the temperature program more accurative

--- the-c-programming-language-second-edition/chapter-01/1.2/main.c
+++ the-c-programming-language-second-edition/chapter-01/1.2/main.c
@@ -28,8 +28,8 @@ main() {
     fahr = lower;
     
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
