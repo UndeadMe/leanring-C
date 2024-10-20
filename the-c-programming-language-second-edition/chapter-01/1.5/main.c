0056fe0cdb74fefc07b8281d53c6607a2bfcb5c4
undeadme <arash.k021@yahoo.com>
2024-10-20 23:24:51 +0330

    feat: add 1.5 portion

---
+++ the-c-programming-language-second-edition/chapter-01/1.5/main.c
@@ -0,0 +1,13 @@
+// Character Input and Output
+// https://www.nielit.gov.in/gorakhpur/sites/default/files/Gorakhpur/OLevel_2_B4_CLang_9Apr_SS.pdf
+
+#include <stdio.h>
+#include <stdbool.h>
+
+main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
+}
