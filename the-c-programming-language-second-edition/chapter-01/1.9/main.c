8ed28fa32ac040aed3e11e3402249f32de87ae25
undeadme <arash.k021@yahoo.com>
2024-10-21 20:58:41 +0330

    feat: add 1.9 portion

---
+++ the-c-programming-language-second-edition/chapter-01/1.9/main.c
@@ -0,0 +1,13 @@
+// Character Arrays
+// https://www.studytonight.com/c/string-and-character-array.php
+// https://www.scaler.com/topics/character-array-in-c/
+// https://www.studytonight.com/c/variables-in-c.php
+// https://www.studytonight.com/c/c-syntax.php
+// https://www.studytonight.com/c/datatype-in-c.php
+// https://www.studytonight.com/c/arrays-in-c
+
+int main() {
+    char str[] = "HOME";
+
+    return 0;
+}
