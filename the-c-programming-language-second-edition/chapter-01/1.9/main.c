8e17500b35a99baf81304cf4465b40e0ee008c4c
undeadme <arash.k021@yahoo.com>
2024-10-22 09:17:08 +0330

    feat: add 1.9 portion

--- the-c-programming-language-second-edition/chapter-01/1.9/main.c
+++ the-c-programming-language-second-edition/chapter-01/1.9/main.c
@@ -5,9 +5,21 @@
 // https://www.studytonight.com/c/c-syntax.php
 // https://www.studytonight.com/c/datatype-in-c.php
 // https://www.studytonight.com/c/arrays-in-c
+// https://www.geeksforgeeks.org/c-program-to-find-the-length-of-a-string/
+
+#include <stdio.h>
 
 int main() {
-    char str[] = "HOME";
+    char dummy_text[] = "HOME";
+    char reverse_text[100];
 
-    return 0;
+    int length = 0;
+
+    while (dummy_text[length] != '\0')
+        ++length;
+
+    for (int i = (length - 1); i >= 0; --i)
+        reverse_text[length - 1 - i] = dummy_text[i];
+
+    printf("%s", reverse_text);
 }
