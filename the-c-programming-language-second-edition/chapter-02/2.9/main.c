a8e831a0e6a1fcd9ba04f696d9888aaa89cf5d7b
undeadme <arash.k021@yahoo.com>
2024-10-27 07:13:21 +0330

    feat: add 2.9 portion

---
+++ the-c-programming-language-second-edition/chapter-02/2.9/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    /*
+        Bitmasking
+        there is 6 bitwise operators
+            - AND
+            - OR
+            - inclusive OR
+            - exclusive OR
+            - left shift
+            - right shift
+            - unary
+    */
+    
+
+    return 0;
+}
