5711aef5cb4f6a3c7069c59e708eb78bce2f76a8
undeadme <arash.k021@yahoo.com>
2024-10-29 08:59:31 +0330

    feat: add 2.12 portion

---
+++ the-c-programming-language-second-edition/chapter-02/2.12/main.c
@@ -0,0 +1,19 @@
+/*
+    precedence: https://en.wikipedia.org/wiki/Order_of_operations
+    plt: https://en.wikipedia.org/wiki/Programming_language_theory
+    associativity: https://en.wikipedia.org/wiki/Associative_property, https://en.wikipedia.org/wiki/Operator_associativity
+*/
+
+int main() {
+    /* 
+        precedence and order of evaluation 
+            - associativity
+                In programming, associativity refers to the order in which operators
+                of the same precedence are evaluated in an expression. It determines how
+                expressions are grouped when they have multiple operators of the same rank.
+            - precedence
+                rank of an operation 
+    */
+   
+    return 0;
+}
